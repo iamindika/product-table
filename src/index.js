40338abd5799a02a335d5ae306188e81cce57d67
iamindika <ikolamba2@gmail.com>
2021-07-08 14:02:30 -0400

    Render SearchBar for test purposes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import SearchBar from './components/SearchBar';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <SearchBar 
+      search='Scruffy' 
+      inStock={true}
+      onChange={(id, e) => alert(`id ${id} is ${e.target.value}`)}
+      onSubmit={() => alert('Form Submitted!')}
+    />
   </React.StrictMode>,
   document.getElementById('root')
 );
