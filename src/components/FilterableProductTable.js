6e35778fafb7d933c455be1c59ef118dbdafa47a
iamindika <ikolamba2@gmail.com>
2021-07-09 05:15:10 -0400

    Move product list from FilterableProductTable to ProductTable component

--- src/components/FilterableProductTable.js
+++ src/components/FilterableProductTable.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 
-const products = [
-  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
-  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
-  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
-  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
-  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
-  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
-];
-
 export default class FilterableProductTable extends React.Component{
   constructor(props) {
     super(props);
@@ -48,7 +39,7 @@ export default class FilterableProductTable extends React.Component{
   }
 
   render() {
-    const { search, inStock } = this.state;
+    const { search, inStock, submit } = this.state;
     return (
       <div className='product-table'>
         <SearchBar 
--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -2,6 +2,15 @@ import React from 'react';
 import ProductCategory from './ProductCategory';
 import ProductItem from './ProductItem';
 
+const products = [
+  {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+  {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+  {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+  {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+  {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+  {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+];
+
 export default function ProductTable({ products, inStock }) {
   const categoryLabels = [];
 
