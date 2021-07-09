f6e134f6178f0ad18ff4f757029ee8a19974ffc1
iamindika <ikolamba2@gmail.com>
2021-07-09 06:26:13 -0400

    Remove form wrapper, onSubmit handlers/props

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,26 +1,24 @@
 import React from 'react';
 
-export default function SearchBar({ search, inStock, onChange, onSubmit }) {
+export default function SearchBar({ search, inStock, onChange }) {
   return (
     <div className='search-bar'>
-      <form onSubmit={onSubmit}>
-        <div className='search-item'>
-          <input 
-            type="text" 
-            value={search}
-            placeholder="Search..." 
-            onChange={(e) => onChange('search', e)}
-          />
-        </div>
-        <div className='search-filter'>
-          <input 
-            type="checkbox" 
-            checked={inStock} 
-            onChange={(e) => onChange('inStock', e)}
-          />
-          <p>Only show products in stock</p>
-        </div>
-      </form>
+      <input
+        id='search-item'
+        type="text" 
+        value={search}
+        placeholder="Search..." 
+        onChange={(e) => onChange('search', e)}
+      />
+      <div>
+        <input 
+          id='search-filter'
+          type="checkbox" 
+          checked={inStock} 
+          onChange={(e) => onChange('inStock', e)}
+        />
+        <p>Only show products in stock</p>
+      </div>
     </div>
   )
 }
